98a9c16b85fef1c9a4adc83798277edae8b121b3
mysmallan <malinmspersson@hotmail.com>
2017-09-27 20:03:03 +0200

    første commit

---
+++ hamburger-menu.js
@@ -0,0 +1,7 @@
+document.querySelector(".hamburger-button").addEventListener("click", onButtonClick);
+
+function onButtonClick(){
+  document.querySelector(".header").classList.toggle("menu-open");
+  document.querySelector(".meny").classList.toggle("visible");
+  document.querySelector(".hamburger-button").classList.toggle("open");
+}
---
+++ ostrem-webpage/hamburger-menu.js
@@ -0,0 +1,6 @@
+document.querySelector(".hamburger-button").addEventListener("click", onButtonClick);
+
+function onButtonClick(){
+  document.querySelector(".meny").classList.toggle("visible");
+}
+
